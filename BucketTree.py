f1acdd95420c730d7ba704171508cc2fc2d77d57
Peixuan Gao <peixuan670@outlook.com>
2018-09-12 16:47:44 -0400

    v0.1 adding Node, Rule and basic functions

--- BucketTree.py
+++ BucketTree.py
@@ -1,4 +1,6 @@
 from Bucket import *
+from Node import *
+from Rule import *
 
 class BucketTree():
     def __init__(self, RuleSet = None):
@@ -27,6 +29,7 @@ class BucketTree():
 
     def split(self, dim):
 
+
     def countPrefix(self):
         tmp = self.bucket.node
         sub = 1 << 7
@@ -36,3 +39,7 @@ class BucketTree():
             sub = sub >> 1
             count += 1
         return count
+
+    def matchRuleAndNode(self, node, rule):
+
+    def matchPrefix(self, addressOne, maskOne, addressTwo):
--- Node.py
+++ Node.py
@@ -1,4 +1,14 @@
 class Node():
-    def __init__(self, prefix = 0, mask = 0):
-        self.prefix = prefix;
+
+    def __init__(self, parentNode = None, address = [0,0,0,0], mask = [0,0,0,0]):
+        self.parentNode = parentNode;
+        self.address = address;
         self.mask = mask;
+
+    address = []        # The 4-tuple address of the node [SrcIP, DstIP, SrcPort, DstPort]
+    mask = []           # The 4-tuple masks of the node [same as above]
+
+    relatedRules = []   # The related rules
+
+    parentNode = None   # The parent Node of this node
+    childNodeList = []  # THe child Nodes of this node
