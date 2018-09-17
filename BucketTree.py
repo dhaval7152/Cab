2caaa717fd6a0151017f2505dd8afd57275ddc21
nyuzyt <nyuzyt@nyu.edu>
2018-09-17 11:33:28 -0400

    v0.4 Move methods from tree to node, update split function

--- BucketTree.py
+++ BucketTree.py
@@ -3,15 +3,19 @@ from Node import *
 from Rule import *
 
 class BucketTree():
-    def __init__(self, RuleSet = None):
-        self.bucket = Bucket()
-        self.childNum = 0
-        self.children = []
+    def __init__(self, RuleSet = None, thr = 5):
+        self.root = Node()
 
         self.dims = self.gen_dim()
         # dim got
 
-        self.splitNode(self.dims)
+        self.root.splitNode(dims, thr)
+
+        self.thr = thr
+
+    root = Node()       # The root node of bucketTree
+    dims = []           # The dimension of cut
+    thr = 0             # The threshold of one bucket
 
     # cal the dim of split
     def gen_dim(self):
@@ -24,84 +28,3 @@ class BucketTree():
                 dims.append(init)
                 init = [0, 0, 0, 0]
         return dims
-
-    # got which split is better
-    def splitNode(self, dims):
-        for dim in dims:
-            cost = self.split(dim)
-            return cost
-
-    def split(self, dim):
-        digits = 0;
-        for i in range(4):
-
-            # this dimension needs one cut
-            if dim[i] == 1:
-                digits = self.countOnes(self.bucket.node.mask[i])
-
-                self.oneDimOneCut();
-
-                
-            # this dimension needs two cut
-            if dim[i] == 2:
-                digits = self.countOnes(self.bucket.node.mask[i])
-
-
-
-
-
-
-        return digits
-
-    def oneDimOneCut(self):
-        tmpChild = Bucket()
-        tmpChild.node.mask = self.bucket.node.mask
-        tmpChild.node.address = self.bucket.node.address
-
-        # Add left child
-        self.childNum += 1
-        tmpChild.node.mask[i] = self.bucket.node.address[i] + 1 << (8 - digits)
-        tmpChild.node.address[i] = self.bucket.node.address[i]
-        self.children.append(tmpChild)
-
-        # Add right child
-        self.childNum += 1
-        tmpChild.node.mask[i] = self.bucket.node.address[i] + 1 << (8 - digits)
-        tmpChild.node.address[i] = self.bucket.node.address[i] + 1 << (8 - digits)
-        self.children.append(tmpChild)
-
-    def oneDimTwoCut(self):
-
-
-    def countOnes(self, tmp):
-        sub = 1 << 7
-        count = 0
-        while tmp > 0:
-            tmp -= sub
-            sub = sub >> 1
-            count += 1
-        return count
-
-    def matchRuleAndNode(self, node, rule):
-
-        if not self.matchPrefix(node.address[0], node.mask[0], rule.address[0], rule.mask[0]):
-            return False
-        if not self.matchPrefix(node.address[1], node.mask[1], rule.address[1], rule.mask[1]):
-            return False
-        if not self.matchPrefix(node.address[2], node.mask[2], rule.address[2], rule.mask[2]):
-            return False
-        if not self.matchPrefix(node.address[3], node.mask[3], rule.address[3], rule.mask[3]):
-            return False
-
-        return True
-
-
-    # Find is two pair of (prefix, mask) overlapping
-    def matchPrefix(self, addressOne, maskOne, addressTwo, maskTwo):
-        maskShort = 0
-        if (maskOne > maskTwo):
-            maskShort = maskTwo
-        else:
-            maskShort = maskOne
-
-        return (addressOne & maskShort) == (addressTwo & maskShort)
--- Cab.py
+++ Cab.py
@@ -2,11 +2,11 @@ from BucketTree import *
 
 class Cab():
     def __init__(self, ruleName):
-        ruleSet = self.getRules(ruleName)
+        ruleSet = self.getRules(ruleName, thr)
         myTree = BucketTree(ruleSet)
 
     # ruleSet pharser       ----- to be done
-    def getRules(self):
+    def getRules(self, ruleName):
         return self
 
     def query(self, address):
